f97b5c90972294d3a9d848577c931343fc6c7b8f
yihaitao <yihaitao@126.com>
2025-04-29 17:14:36 +0800

    管理前端+微信用户前端

---
+++ miniprogram-1/pages/index/index.js
@@ -0,0 +1,45 @@
+// index.js
+Page({
+  data:{
+    msg:"Hello,Weixin!",
+    nickName:'',
+    avatarUrl:'',
+    code:'',
+    result:''
+  },
+  getUserInfo:function(){
+    wx.getUserProfile({
+      desc: '获取用户信息',
+      success:(res)=> {
+        console.log(res);
+        this.setData({
+          avatarUrl:res.userInfo.avatarUrl,
+          nickName: res.userInfo.nickName
+        })
+      } 
+    })
+  },
+  wxlogin:function(){
+        wx.login({
+          success: (res) => {
+            console.log("授权码："+res.code)
+            this.setData({
+              code:res.code
+            })
+          }
+        })
+  },
+
+  sendRequest:function(){
+        wx.request({
+          url: 'http://localhost:8080/user/shop/status',
+          method:'GET',
+          success:(res) => {
+            console.log("响应结果：" + res.data.data)
+            this.setData({
+              result:res.data.data
+            })
+          }
+        })
+      }
+})
